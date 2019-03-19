085229e22fcd813c47a6ca8703d4ba4b4e5a9d40
LiuJuiHung <ray9956666@gmail.com>
2019-03-19 19:59:47 +0800

    Modify Main.c and Observe Register State
    Insert function "sum" and "func"

--- main.c
+++ main.c
@@ -1,5 +1,26 @@
 void reset_handler(void)
 {
+	int a = 3;
+	int b = 6;
+	int c = 15;
+	int d = 4;
+	int e = 23;
+	sum(a, b);
+	func(a, b, c, d, e);
 	while (1)
 		;
 }
+
+int sum(int d, int e)
+{
+	int f = d + e;
+	return f;
+}
+
+int func(int aa, int bb, int cc, int dd, int ee)
+{
+	int ff = aa + bb + cc + dd + ee;
+	return ff;
+}
+
+
